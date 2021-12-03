67d5394dd59a2bb2655ab49b1d1257876e2e584e
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-12-03 21:52:29 +0800

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,5 @@
+//[Question Text, Button 1 Text, Button 2 Text, Button 1 Color, Button 2 Color, Final Question?] (Question after Button 1 Pressed, Question after Button 2 Pressed)
+q1 = ["This is a question", "Yes, indeed", "No, it isn't", "#CD5C5C", "#F0E68C", "false"];
+q1.push(q2, q3);
+
+q2 = ["Question 1", "Yes", "No", "true"];
