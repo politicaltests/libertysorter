354803287de6b5370391766c0cd797cb2f7e7e20
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-12-03 23:25:49 +0800

    Update questions.js

--- questions.js
+++ questions.js
@@ -3,7 +3,7 @@ q1 = ["All crimes that are victimless, like drug consumption, are not crimes.", 
 q1.push(q10, q3);
 
 q10 = ["test like drug consumption, are not crimes.", "Agree", "Disagree", "#3772ff", "#df2935", "true", "true"]; 
-q1.push(q2, q3);
+q10.push(q2, q3);
 
 q2 = ["What should be the relationship between the federal government of a country, and its subdivisions?", 
       "The Federal Government should be subservient to its subdivisions", "The Federal Government should have the final authority", "#ff9f1c", "#2ec4b6", "false"];
