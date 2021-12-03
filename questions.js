b2f98391310554e4e8c74eac9fa9281a0e1e2c51
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-12-03 22:03:58 +0800

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,5 +1,10 @@
 //[Question Text, Button 1 Text, Button 2 Text, Button 1 Color, Button 2 Color, Final Question?] (Question after Button 1 Pressed, Question after Button 2 Pressed)
-q1 = ["This is a question", "Yes, indeed", "No, it isn't", "#CD5C5C", "#F0E68C", "false"];
+q1 = ["All crimes that are victimless, like drug consumption, are not crimes.", "Agree", "Disagree", "#3772ff", "#df2935", "false", "false"]; 
 q1.push(q2, q3);
 
-q2 = ["Question 1", "Yes", "No", "true"];
+q2 = ["What should be the relationship between the federal government of a country, and its subdivisions?", 
+      "The Federal Government should be subservient to its subdivisions", "The Federal Government should have the final authority", "#ff9f1c", "#2ec4b6", "false"];
+q2.push(q4, q5);
+
+q3 = ["All crimes that are victimless, like drug consumption, are not crimes.", "Agree", "Disagree", "#3772ff", "#df2935", "false"]; 
+q1.push(q2, q3);
