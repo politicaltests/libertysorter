039c26b719ddac2f2b0dd49ea51144e2823ca1f3
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-12-04 23:29:18 +0800

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 //[Question Text, Button 1 Text, Button 2 Text, Button 1 Color, Button 2 Color, Final Question?] (Question after Button 1 Pressed, Question after Button 2 Pressed)
 var q1 = ["All crimes that are victimless, like drug consumption, are not crimes.", "Agree", "Disagree", "#3772ff", "#df2935", "false", "false"]; 
-q1.push(q10, q10);
+q1.push("q10", "q10");
 
 var q10 = ["test like drug consumption, are not crimes.", "Agree", "Disagree", "#3772ff", "#df2935", "true", "true"]; 
 q10.push(q2, q3);
