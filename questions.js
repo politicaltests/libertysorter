85d1d4e0a3f759308eab6625d3375724359cfbfa
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-12-04 11:10:08 +0800

    Update questions.js

--- questions.js
+++ questions.js
@@ -5,8 +5,7 @@ q1.push(q10, q3);
 q10 = ["test like drug consumption, are not crimes.", "Agree", "Disagree", "#3772ff", "#df2935", "true", "true"]; 
 q10.push(q2, q3);
 
-q2 = ["What should be the relationship between the federal government of a country, and its subdivisions?", 
-      "The Federal Government should be subservient to its subdivisions", "The Federal Government should have the final authority", "#ff9f1c", "#2ec4b6", "false"];
+q2 = ["What should be the relationship between the federal government of a country, and its subdivisions?", "The Federal Government should be subservient to its subdivisions", "The Federal Government should have the final authority", "#ff9f1c", "#2ec4b6", "false"];
 q2.push(q4, q5);
 
 q3 = ["All crimes that are victimless, like drug consumption, are not crimes.", "Agree", "Disagree", "#3772ff", "#df2935", "false"]; 
