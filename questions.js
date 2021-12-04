59cf7836c2c70445fb66657b3e264244027da51d
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-12-04 12:10:27 +0800

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,12 +1,12 @@
 //[Question Text, Button 1 Text, Button 2 Text, Button 1 Color, Button 2 Color, Final Question?] (Question after Button 1 Pressed, Question after Button 2 Pressed)
-q1 = ["All crimes that are victimless, like drug consumption, are not crimes.", "Agree", "Disagree", "#3772ff", "#df2935", "false", "false"]; 
+var q1 = ["All crimes that are victimless, like drug consumption, are not crimes.", "Agree", "Disagree", "#3772ff", "#df2935", "false", "false"]; 
 q1.push(q10, q3);
 
-q10 = ["test like drug consumption, are not crimes.", "Agree", "Disagree", "#3772ff", "#df2935", "true", "true"]; 
+var q10 = ["test like drug consumption, are not crimes.", "Agree", "Disagree", "#3772ff", "#df2935", "true", "true"]; 
 q10.push(q2, q3);
 
-q2 = ["What should be the relationship between the federal government of a country, and its subdivisions?", "The Federal Government should be subservient to its subdivisions", "The Federal Government should have the final authority", "#ff9f1c", "#2ec4b6", "false"];
+var q2 = ["What should be the relationship between the federal government of a country, and its subdivisions?", "The Federal Government should be subservient to its subdivisions", "The Federal Government should have the final authority", "#ff9f1c", "#2ec4b6", "false"];
 q2.push(q4, q5);
 
-q3 = ["All crimes that are victimless, like drug consumption, are not crimes.", "Agree", "Disagree", "#3772ff", "#df2935", "false"]; 
+var q3 = ["All crimes that are victimless, like drug consumption, are not crimes.", "Agree", "Disagree", "#3772ff", "#df2935", "false"]; 
 q1.push(q2, q3);
