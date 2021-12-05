5129ebf0c80bddada81f6a580a1db1aaec571bf9
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-12-05 16:54:43 +0800

    Update questions.js

--- questions.js
+++ questions.js
@@ -9,28 +9,28 @@ var q2 = ["Are monarchies inherently contradictory to the principles of freedom 
 var q3 = ["Within a federal state, what should be the relationship between the federal government of a country, and its subdivisions?", 
           "The Federal Government should be subservient to its subdivisions", "The Federal Government should have the ultimate authority", 
           "#0f084b", "#a6cfd5", "antifed", "false",
-          function() { init_question(none) }, function() { init_question(q4) } ];
+           results, function() { init_question(q4) } ];
 
 var q4 = ["Ideally, how powerful should the central government be?", "Strong, in order to promote national unity", 
           "Relatively weak, with limited power", "#121420", "#394c6a", "hamilton", "false",
-           function() { init_question(none) }, function() { init_question(q5) } ];
+           results, function() { init_question(q5) } ];
 
 var q5 = ["What should the economy focus more on?", "Agriculture", "Industries", "#f2cd5d", "#6c7d47", "jefferson", "false",
-           function() { init_question(none) }, function() { init_question(q6) } ];
+           results, function() { init_question(q6) } ];
 
 var q6 = ["How is the value of products determined?", "By a natural sum of labor, profits, and expenditures", 
           "Through trading, value is subjective", "#5398be", "#dea54b", "adamsmith", "false",
-           function() { init_question(none) }, function() { init_question(q7) } ];
+           results, function() { init_question(q7) } ];
 
 var q7 = ["Is it ideal for countries to intervene in other countries, in order to protect and preserve democracy abroad?", 
           "Generally, yes", "No, in most, if not all cases", "#683257", "#716f71", "goldwater", "false",
-           function() { init_question(none) }, function() { init_question(q8) } ];
+           results, function() { init_question(q8) } ];
 
 var q8 = ["Are you strongly against prohibition of drugs and alcohol?", "Yes", "No", "#2d728f", "#f49e4c", "mises", "coolidge",
-           function() { init_question(none) }, function() { init_question(none) } ];
+           results, results ];
 
 var q9 = ["Which type of monarchy is more preferable?", "Absolute Monarchy", "Constitutional Monarchy", "#593f62", "#5f7995", "voltaire", "false",
-           function() { init_question(none) }, function() { init_question(q10) } ];
+           results, function() { init_question(q10) } ];
 
 var q10 = ["Where does knowledge primarily come from?", "Sensory Experience", "The sense of reason", "#053b06", "#0b5d1e", "locke", "rosseau",
-           function() { init_question(none) }, function() { init_question(none) } ];
+           results, results ];
